ed4f7628a664affc2c6786b8bbff68a019c17a59
nekkochan <nekkochan@gmail.com>
2024-12-29 15:53:53 +0700

    Update codebase

--- dataset.f90
+++ dataset.f90
@@ -1,4 +1,7 @@
 MODULE DATASET
     IMPLICIT NONE
-
+    TYPE :: Dataset_t
+        TYPE(Tensor_t), ALLOCATABLE :: data(:)
+        TYPE(Tensor_t), ALLOCATABLE :: labels(:)
+    END TYPE Dataset_t
 END MODULE DATASET
